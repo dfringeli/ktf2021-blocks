46e18008a2739f9abfd53111f704e2c3220a7c85
dominik.fringeli <dominik.fringeli@students.fhnw.ch>
2019-01-21 22:52:29 +0100

    added width: 100% to have the same width for every column works for 1, 2 and 3 columns on desktop and mobile close #1

--- src/columns/column.js
+++ src/columns/column.js
@@ -33,7 +33,7 @@ const settings = {
 	},
 
 	save() {
-		return <div class="flex-fill m-2"><InnerBlocks.Content /></div>;
+		return <div class="flex-fill m-2 w-100"><InnerBlocks.Content /></div>;
 	},
 };
 
