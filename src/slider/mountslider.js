9b0e5cd7c8b92f951ff9c694e895345b61c8a531
dfringeli <dfringeli@outlook.com>
2020-01-24 11:01:22 +0100

    fixed safari js lamda bug

--- src/slider/mountslider.js
+++ src/slider/mountslider.js
@@ -1,8 +1,6 @@
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-sleep(100).then(() => {
-    var glide = new Glide('.ktf2021-slider', { type: 'carousel', autoplay: 3000, swipeThreshold: false, dragThreshold: false });
-    glide.mount();
+jQuery(document).ready(function () {
+    setTimeout(function () {
+        var glide = new Glide('.ktf2021-slider', { type: 'carousel', autoplay: 3000, swipeThreshold: false, dragThreshold: false });
+        glide.mount();
+    }, 100);
 });
