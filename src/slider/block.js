6196aec55db119dbd3f55c5ea68b6377954129c1
Dominik Fringeli <dfringeli@outlook.com>
2020-04-04 09:20:35 +0200

    added deprecated

--- src/slider/block.js
+++ src/slider/block.js
@@ -295,4 +295,77 @@ registerBlockType('ktf2021/ktf2021-slider', {
 			return null;
 		}
 	},
+	deprecated: [
+		{
+            migrate() {
+                return {
+                    watermarktext: "mir sy turnfescht! bisch du's au?"
+                };
+			},
+			
+			attributes: {
+				images: {
+					source: "query",
+					default: [],
+					selector: "li.glide__slide",
+					query: {
+						imageSrc: {
+							source: "attribute",
+							selector: "img.ktf2021-slide-image",
+							attribute: "src"
+						},
+						index: {
+							source: "attribute",
+							selector: "div.ktf2021-slide-image-container",
+							attribute: "data-index"
+						}
+					}
+				}
+			},
+		
+			save({ attributes }) {
+				const { images } = attributes;
+		
+				if (images.length > 0) {
+		
+					const bullets = images.map(function (image, index) {
+						return (
+							<button className={'glide__bullet'} key={image.index} data-glide-dir={'=' + index}></button>
+						);
+					});
+		
+					const slides = images.map(function (image) {
+						return (
+							<li className={'glide__slide'} key={image.index}>
+								<div className={'ktf2021-slide-image-container'} data-index={image.index}>
+									<img className={'ktf2021-slide-image'} src={image.imageSrc} />
+								</div>
+							</li>
+						);
+					});
+		
+					return (
+						<div className="ktf2021-container-white p-0">
+							<div className="ktf2021-content-fullwidth p-0">
+								<div className="glide ktf2021-slider">
+									<div className="glide__track" data-glide-el="track">
+										<ul className="glide__slides">
+											{slides}
+										</ul>
+									</div>
+									<div className="glide__bullets" data-glide-el="controls[nav]">
+										{bullets}
+									</div>
+								</div>
+							</div>
+							<ScriptTag type="text/javascript" src="https://ktf21.ch/wp-content/plugins/ktf2021-blocks/src/slider/mountslider.js" />
+						</div>
+					);
+				} else {
+					return null;
+				}
+			},
+
+		}
+	]
 });
